959e376f1c46320ad5860aaf1a789ff87c661323
LChuang <1814334133@qq.com>
2023-04-10 10:55:29 +0800

    Initial commit

---
+++ src/com/lc/test/Test.java
@@ -0,0 +1,12 @@
+package com.lc.test;
+
+/**
+ * @author LiChuang
+ * @date 2023/4/8
+ * @desc
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello gitHub");
+    }
+}
